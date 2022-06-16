831ba05ee0b8051e5cedb56fcaf868ec22d91c05
Mathew Adewale <maaadewale@yahoo.com>
2022-06-16 12:16:37 +1000

    code

---
+++ assets/js/myquestions.js
@@ -0,0 +1,38 @@
+let questions = [
+    {
+        numb: 1,
+        question: "which of the following is the easiest programming language to learn?",
+        answer: 'HTML',
+        options: [
+          'HTML',
+          'Java', 
+          'JavaScript', 
+          'Python'
+        ]
+      },
+    
+      {
+        numb: 2,
+        question: 'The following programming language, except ------------ can be use for website front end development?',
+        answer: 'GIT',
+        options: [
+          'HTML',
+          'Java', 
+          'JavaScript', 
+          'GIT'
+        ]
+      },
+    
+    {
+        numb: 3,
+        question: 'which of the following programing language is easy to use for text appearance?',
+        answer: 'CSS',
+        options: [
+         'HTML',
+         'CSS', 
+         'JavaScript', 
+         'Python'
+        ]
+    }
+
+]
